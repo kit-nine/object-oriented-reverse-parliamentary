e8ec8786efbb02486520ef839caa3bbff5b33469
Kit <victoriaseigler@outlook.com>
2022-09-09 11:57:50 -0400

    commit

--- graphing test.py
+++ graphing test.py
@@ -1,47 +1,43 @@
-"""r1,r2,r3,r4,r5,r6,r7,r8,r9,r10 = (100,60), (90,50), (80,40), (70,50), (60,50), (50,40), (40,30), (30,30), (20,20), (10,10)
-points = [r1,r2,r3,r4,r5,r6,r7,r8,r9,r10]
-columns = [100,90,80,70,60,50,40,30,20,10]
+# y - position (two graphs displayed for each position; one graph used for left-right and the other for authoritarian-libertarian)
+# x - time
+# center - (0,0)
 
-for b in columns:
-    row_points = []
-    for a in points:
-        if a[1] == b:
-            row_points.append(a)
-    if row_points != []:
-        after_points = int(len(columns) - (row_points[0][0] / 10))
-        middle_points = 
-
-        before_points = len(columns) - len(row_points) - after_points
-        print(b, "	|", " ░"*before_points, " ▓"*len(row_points), " ░"*after_points, sep="")
-    else: print(b, "	|", " ░"*len(columns), sep="")"""
-
-grid = []
-printable_grid = []
-cols = 10
-rows = 10
-
-r1,r2,r3,r4,r5,r6,r7,r8,r9,r10 = (60,100), (50,90), (40,80), (50,70), (50,60), (40,50), (30,40), (30,30), (20,20), (10,10)
-points = [r1,r2,r3,r4,r5,r6,r7,r8,r9,r10]
-
-for a in range(cols):
-    temp = []
-    for b in range(rows):
-        temp.append(b)
-    grid.append(temp)
-for a in range(cols):
-    for c in points:
-        if c[1] == a*10:
-            grid[int((c[0] / 10) - 1)][int((c[1] / 10) - 1)] = "▓"
-for a in range(rows):
-    temp = []
-    for b in range(cols):
-        temp.append(grid[a][b])
-    printable_grid.append(temp)
-for a in range(rows):
-    print(str(100-(a*10)), " |", sep="", end="")
-    for b in range(cols):
-        if printable_grid[a][b] == "▓":
-            print("▓ ", end="")
+# this should be similar to the optimal graph
+# so it is the example graph this test will use
+# |-------------------------|
+# | ░░░░░░░░░░ | ░░░░░░░░░░ |
+# | ▓░░░░░░░░░ | ░░░░░░░░░░ |
+# | ░░░░░░░░░░ | ░░░░░░░░░░ |
+# | ░▓░░░░░░░░ | ░░░░░░░░░░ |
+# | ░░▓░░░░░▓░ | ░░▓▓▓▓▓░░░ |
+# |------------|------------|
+# | ░░░░░░▓▓░▓ | ▓▓░░░░░▓▓▓ |
+# | ░░░▓░░░░░░ | ░░░░░░░░░░ |
+# | ░░░░░▓░░░░ | ░░░░░░░░░░ |
+# | ░░░░▓░░░░░ | ░░░░░░░░░░ |
+# | ░░░░░░░░░░ | ░░░░░░░░░░ |
+# |-------------------------|
+points = [(-10,4), (-9,2), (-8,1), (-7,-2), (-6,-4), (-5,-3), (-4,-1), (-3,-1), (-2,1), (-1,-1), (0,-1), (1,-1), (2,-1), (3,1), (4,1), (5,1), (6,1), (7,1), (8,-1), (9,-1), (10,-1)]
+print("┌──────────┬──────────┐")
+for a in range(5):
+    print("│", end="")
+    for b in range(len(points)):
+        if b == 10:
+            print("│", end="")
+        elif points[b][1] == 5 - a:
+            print("▓", end="")
         else:
-            print("░ ", end="")
-    print("\n", end="")
+            print("░", end="")
+    print("│\n", end="")
+print("├──────────┼──────────┤")
+for a in range(5):
+    print("│", end="")
+    for b in range(len(points)):
+        if b == 10:
+            print("│", end="")
+        elif points[b][1] == -1 * (a + 1):
+            print("▓", end="")
+        else:
+            print("░", end="")
+    print("│\n", end="")
+print("└──────────┴──────────┘")
